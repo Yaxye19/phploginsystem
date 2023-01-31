6c4f23ac6d97eeed1ac3df931cc8a0b4b832541c
Yaxye19 <yaxyeali12@outlook.com>
2023-01-31 21:13:34 +0000

    added starting files

---
+++ blog.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<section class="contact" id="contact">
+    <div class="title">
+      <h2 class="section-title">Contact Me</h2>
+    </div>
+    <div class="content">
+      <div class="row">
+        <div class="card">
+          <div class="info">
+            <h3>Address</h3>
+            <span>Leicester, UK, England</span>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3>Phone</h3>
+            <span>07549821164</span>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3>Email Address</h3>
+            <span>yaxyeali12@outlook.com</span>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3>Website</h3>
+            <span>yaxyeali.com</span>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="contact-form">
+          <h3>Send Message</h3>
+          <div class="input-box">
+            <input type="text" name="box" value="" placeholder="Name">
+          </div>
+          <div class="input-box">
+            <input type="text" name="inbox" value="" placeholder="Email">
+          </div>
+          <div class="input-box">
+            <textarea name="name" rows="8" cols="80" placeholder="Message"></textarea>
+          </div>
+          <div class="input-box">
+            <input type="submit" class="send-btn" name="subbox" value="Send" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Login System</title>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <a href="index.php" class="brand">「YAXYE」</a>
+    <div class="navigation">
+      <a href="index.php">Home</a>
+      <a href="discover.php">About Us</a>
+      <a href="blog.php">Find Yaxye</a>
+      <a href="signup.php">Sign Up</a>
+      <a href="login.php">Log in</a>
+    </div>
+  </header>
+
+  
+  <section class="main" id="main">
+    <div class="content">
+      <h2>Hello, I'm<br><span>Yaxye Ali</span></h2>
+      <div class="animated-text">
+        <h3>Student</h3>
+        <h3>Web Developer</h3>
+        <h3>FreeLancer</h3>
+      </div>
+      <a href="https://github.com/Yaxye19" target="_blank" rel="noopener noreferrer" class="btn">See My Github</a>
+    </div>
+  </section>
+
+
+
+    
+</body>
+</html>
+
+
